1b228bc4b5bc14846e16a0d702cec1c0d9f11cf1
zeppi <jessopb@gmail.com>
2021-09-08 02:51:30 -0400

    working tomato

---
+++ components/timerContainer.js
@@ -0,0 +1,10 @@
+
+export default function Timer(props){
+    const { loggedIn } = props;
+    return (
+        <div class="md:flex items-center space-x-1">
+            A Timer
+        </div>
+
+    )
+}
